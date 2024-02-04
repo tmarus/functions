1bb7eef22223813a5d3725bc5d6d475dd8cb25a2
Marek <158512833+tmarus@users.noreply.github.com>
2024-02-04 22:05:44 +0100

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,20 @@
+from math import pi
+
+def kwadrat(x):
+    print(f"{x * x}")
+
+def prostokat(x, y):
+    print(f"{x * y}")
+
+def trojkat(x, y):
+    print(f"{x * y / 2}")
+
+def trapez(x, y, z):
+    print(f"{x + y * z / 2}")
+
+def romb(x, y):
+    print(f"{x * y}")
+
+def kolo(x):
+    print(f"{pi * (x ** 2)}")
+
